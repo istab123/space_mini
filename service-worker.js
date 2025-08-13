632373b12a3ecc6aab652cb2afe339dc06f33abc
istab123 <Sebastian.Burger1@outlook.de>
2025-08-13 11:58:59 +0200

    Remove binary icon and inline manifest graphic

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'space-mini-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/constants.js',
+  '/audio.js',
+  '/storage.js',
+  '/game.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
